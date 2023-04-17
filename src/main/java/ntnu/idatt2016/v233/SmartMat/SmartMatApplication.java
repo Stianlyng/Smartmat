afc758f6efa2abc2e6fc274f8cf568fdf07310a4
Pedro Cardona <pedropca@stud.ntnu.no>
2023-04-17 10:15:13 +0200

    Main

---
+++ src/main/java/ntnu/idatt2016/v233/SmartMat/SmartMatApplication.java
@@ -0,0 +1,13 @@
+package ntnu.idatt2016.v233.SmartMat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartMatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ntnu/idatt2016/v233/SmartMat/SmartMatApplicationTests.java
@@ -0,0 +1,13 @@
+package ntnu.idatt2016.v233.SmartMat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
