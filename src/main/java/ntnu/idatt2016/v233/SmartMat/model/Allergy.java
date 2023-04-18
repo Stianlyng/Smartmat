2c600b65a6997739bbafbf2e956758b3c01033c1
Stian Lyng <stianlyng@protonmail.com>
2023-04-18 13:52:13 +0200

    add model

---
+++ src/main/java/ntnu/idatt2016/v233/SmartMat/model/Allergy.java
@@ -0,0 +1,13 @@
+package ntnu.idatt2016.v233.SmartMat.model;
+
+/**
+ * This is a record class representing an allergy
+ * 
+ * @author Stian Lyng
+ * @version 1.0
+ *
+ * @param name The name of the allergy
+ * @param description The description of the allergy
+ */
+public record Allergy(String name, String description) {
+}
