f4a4efc15f2ca3e8fcef146e7e93da974afbf97d
harryTheWizzard <hp324245@gmail.com>
2023-04-18 11:34:24 +0200

    added basic filterchain

--- src/main/java/ntnu/idatt2016/v233/SmartMat/config/SecurityConfig.java
+++ src/main/java/ntnu/idatt2016/v233/SmartMat/config/SecurityConfig.java
@@ -1,4 +1,43 @@
 package ntnu.idatt2016.v233.SmartMat.config;
 
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@AllArgsConstructor
+
+/**
+ * Configs for security and authentication
+ * @author Birk
+ */
 public class SecurityConfig {
+
+    /**
+     * Configures the HttpSecurity for the application.
+     * Dose not need ot have csrf enabled, because we are using jwt
+     * and the application is stateless
+     * @param http HttpSecurity to configure
+     * @return SecurityFilterChain with configured HttpSecurity
+     * @throws Exception if an error occurs
+     */
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors().and()
+                .csrf().disable()
+                .authorizeHttpRequests(auth-> auth
+                        .requestMatchers(HttpMethod.POST, "/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+    
 }
