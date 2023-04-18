40aee5160c369a9273b12e0fe4f6248d663e2e77
Anders Austlid <andemau@ntnu.no>
2023-04-18 12:05:01 +0200

    OAuth2 config/utilities added

---
+++ src/main/java/ntnu/idatt2016/v233/SmartMat/util/rsa/Jwks.java
@@ -0,0 +1,36 @@
+package ntnu.idatt2016.v233.SmartMat.util.rsa;
+
+import com.nimbusds.jose.jwk.RSAKey;
+import java.security.KeyPair;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
+
+/**
+ * Jwks is a utility class for generating RSA keys for JSON Web Key Set (JWKS) support.
+ * It provides a method to generate an RSA key pair and create an RSAKey object.
+ *
+ * <p>This class cannot be instantiated.</p>
+ */
+public class Jwks {
+
+    /**
+     * Private constructor to prevent instantiation of this utility class.
+     */
+    private Jwks() {}
+
+    /**
+     * Generates an RSA key pair, creates an RSAKey object, and assigns a unique key ID to it.
+     *
+     * @return An RSAKey object with a public key, private key, and a unique key ID.
+     */
+    public static RSAKey generateRsa() {
+        KeyPair keyPair = KeyGeneratorUtils.generateRsaKey();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        return new RSAKey.Builder(publicKey)
+                .privateKey(privateKey)
+                .keyID(UUID.randomUUID().toString())
+                .build();
+    }
+}
---
+++ src/main/java/ntnu/idatt2016/v233/SmartMat/util/rsa/KeyGeneratorUtils.java
@@ -0,0 +1,38 @@
+package ntnu.idatt2016.v233.SmartMat.util.rsa;
+
+import org.springframework.stereotype.Component;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+
+/**
+ * KeyGeneratorUtils is a utility class for generating key pairs, specifically RSA key pairs.
+ * This class is marked as a Spring component to allow its use in the Spring framework.
+ *
+ * <p>This class cannot be instantiated.</p>
+ */
+@Component
+final class KeyGeneratorUtils {
+
+    /**
+     * Private constructor to prevent instantiation of this utility class.
+     */
+    private KeyGeneratorUtils() {}
+
+    /**
+     * Generates an RSA key pair of 2048 bits.
+     *
+     * @return A KeyPair object containing an RSA public key and private key.
+     * @throws IllegalStateException If an error occurs during key pair generation.
+     */
+    static KeyPair generateRsaKey() {
+        KeyPair keyPair;
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            keyPair = keyPairGenerator.generateKeyPair();
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        return keyPair;
+    }
+}
