78f84fd21b84665aea77d09326177b1d89ee9095
Anders Austlid <andemau@ntnu.no>
2023-04-18 14:11:03 +0200

    Fridge model record class added

---
+++ src/main/java/ntnu/idatt2016/v233/SmartMat/model/Fridge.java
@@ -0,0 +1,14 @@
+package ntnu.idatt2016.v233.SmartMat.model;
+
+/**
+ * Fridge is a record class representing a fridge in the system.
+ *
+ * @author Anders
+ * @version 1.0
+ * @since 04.04.2023
+ *
+ * @param fridgeId
+ * @param groupId
+ */
+public record Fridge(long fridgeId, long groupId) {
+}
