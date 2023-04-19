14fe44c6d38d9b3c8ee3b76e6d5cdc80ac8b31a4
birkon <birkon@stud.ntnu.no>
2023-04-19 14:27:52 +0200

    added test for AllergyRepo

--- src/main/java/ntnu/idatt2016/v233/SmartMat/repository/AllergyRepository.java
+++ src/main/java/ntnu/idatt2016/v233/SmartMat/repository/AllergyRepository.java
@@ -10,17 +10,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * Repository for allergies
  * 
  * @author Stian Lyng
- * @version 1.0
- * @since 04.04.2023
+ * @version 1.2
+ * @since 19.04.2023
  */
 public interface AllergyRepository extends JpaRepository<Allergy, String> {
-
-    /**
-     * Gets an allergy by name
-     *
-     * @param name the name of the allergy
-     * @return an optional containing the Allergy if it exists
-     */
-    List<Allergy> findByName(String name);
 
     }
