6cf98b4aca61acdfa915e157c9582ac477f03393
Anders Austlid <andemau@ntnu.no>
2023-04-20 13:04:27 +0200

    Moved Group entity class to groups package folder

--- src/main/java/ntnu/idatt2016/v233/SmartMat/repository/group/GroupRepository.java
+++ src/main/java/ntnu/idatt2016/v233/SmartMat/repository/group/GroupRepository.java
@@ -4,27 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import ntnu.idatt2016.v233.SmartMat.entity.Group;
 
-import java.util.Optional;
-
 /**
  * Repository for groups
  * 
- * @author Stian Lyng, Anders Austlid
- * @version 1.1
- * @since 20.04.2023
+ * @author Stian Lyng
+ * @version 1.0
+ * @since 19.04.2023
  */
 public interface GroupRepository extends JpaRepository<Group, Long> {
-    /**
-     * Finds a group by its name
-     * @param groupName the name of the group to look up
-     * @return an optional containing the group if it exists
-     */
-    Optional<Group> findByGroupName(String groupName);
 
-    /**
-     * Finds a group by its id
-     * @param groupId the id of the group to look up
-     * @return an optional containing the group if it exists
-     */
-    Optional<Group> findByGroupId(long groupId);
 }
