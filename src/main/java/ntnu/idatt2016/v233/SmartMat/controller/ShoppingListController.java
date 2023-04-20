52230da4dda700fe3d6aded6aff76e0616f85d61
Anders Austlid <andemau@ntnu.no>
2023-04-20 10:09:14 +0200

    Added Swagger OpenAPI depdendency, Spring validator

--- src/main/java/ntnu/idatt2016/v233/SmartMat/controller/ShoppingListController.java
+++ src/main/java/ntnu/idatt2016/v233/SmartMat/controller/ShoppingListController.java
@@ -5,12 +5,9 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 import lombok.AllArgsConstructor;
 import ntnu.idatt2016.v233.SmartMat.dto.request.ShoppingListRequest;
 import ntnu.idatt2016.v233.SmartMat.entity.ShoppingList;
