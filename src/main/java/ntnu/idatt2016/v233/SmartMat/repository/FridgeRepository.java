9b54398deae906db34017e4b3bdc4f97815de40a
Stian Lyng <stianlyng@protonmail.com>
2023-04-21 14:18:59 +0200

    add fridge repository

---
+++ src/main/java/ntnu/idatt2016/v233/SmartMat/repository/FridgeRepository.java
@@ -0,0 +1,15 @@
+package ntnu.idatt2016.v233.SmartMat.repository;
+
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Repository for Products
+ * @author Stian
+ * @version 1.0
+ */
+public interface FridgeRepository extends JpaRepository<Product, Long> {
+
+}
