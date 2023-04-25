8d1470c94112bef9f4e604f7c31882a1789227a0
Stian Lyng <stianlyng@protonmail.com>
2023-04-25 13:51:55 +0200

    implement allergy controller

---
+++ src/main/java/ntnu/idatt2016/v233/SmartMat/dto/response/AllergyResponse.java
@@ -0,0 +1,18 @@
+package ntnu.idatt2016.v233.SmartMat.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * This class represents a response for a weekly menu.
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AllergyResponse {
+    private String name;
+    private String description;
+}
