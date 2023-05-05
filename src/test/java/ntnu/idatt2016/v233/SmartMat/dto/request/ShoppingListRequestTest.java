93034030b46cf9393a53c86baad018a6dff7c10d
birkon <birkon@stud.ntnu.no>
2023-05-05 15:01:50 +0200

    test for request dtos

---
+++ src/test/java/ntnu/idatt2016/v233/SmartMat/dto/request/ShoppingListRequestTest.java
@@ -0,0 +1,46 @@
+package ntnu.idatt2016.v233.SmartMat.dto.request;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShoppingListRequestTest {
+
+    @Test
+    void testEquals() {
+        ShoppingListRequest shoppingListRequest = ShoppingListRequest.builder()
+                .groupID(1)
+                .build();
+
+        ShoppingListRequest shoppingListRequest1 = ShoppingListRequest.builder()
+                .groupID(1)
+                .build();
+
+        assertEquals(shoppingListRequest, shoppingListRequest1);
+
+        shoppingListRequest1.setGroupID(2);
+
+        assertNotEquals(shoppingListRequest, shoppingListRequest1);
+
+        assertEquals(shoppingListRequest, shoppingListRequest);
+
+
+    }
+
+    @Test
+    void testHashCode() {
+        ShoppingListRequest shoppingListRequest = ShoppingListRequest.builder()
+                .groupID(1)
+                .build();
+
+        ShoppingListRequest shoppingListRequest1 = ShoppingListRequest.builder()
+                .groupID(1)
+                .build();
+
+        assertEquals(shoppingListRequest.hashCode(), shoppingListRequest1.hashCode());
+
+        shoppingListRequest1.setGroupID(2);
+
+        assertNotEquals(shoppingListRequest.hashCode(), shoppingListRequest1.hashCode());
+    }
+}
